633c039276352fdb8764f76876934c32085b3973
Ramiro Lugo <ramiro.lugo@mi.unc.edu.ar>
2024-09-15 12:54:47 -0300

    DEM-15 feat: ejemplo de componente especifico para el home

--- src/containers/home/Home.tsx
+++ src/containers/home/Home.tsx
@@ -1,9 +1,11 @@
+import CardHome from "./components/CardHome";
 function Home() {
     return (
         <div className="flex h-[100vh] w-full items-center justify-center">
-            <h1 className="text-3xl font-extrabold uppercase">
-                El switcher | HOME
-            </h1>
+            <CardHome
+                title="El Switcher"
+                description="Esta es la pagina home"
+            />
         </div>
     );
 }
