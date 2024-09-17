693a214c8787f74b6193c0c5eabc241263d635c2
ramiro-l <ramiro.lugo@mi.unc.edu.ar>
2024-09-17 12:25:12 -0300

    DEM-8 fix: se borro el componte de ejemplo en el home.

--- src/containers/home/Home.tsx
+++ src/containers/home/Home.tsx
@@ -1,11 +1,8 @@
-import CardHome from "./components/CardHome";
+import CrearPartida from "./components/CrearPartida";
 function Home() {
     return (
         <div className="flex h-[100vh] w-full items-center justify-center">
-            <CardHome
-                title="El Switcher"
-                description="Esta es la pagina home"
-            />
+            <CrearPartida /> 
         </div>
     );
 }
