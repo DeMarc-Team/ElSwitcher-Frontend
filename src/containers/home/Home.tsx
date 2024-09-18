9226bf4f295a64120760564550393135c944620a
Ramiro Lugo <ramiro.lugo@mi.unc.edu.ar>
2024-09-18 19:24:23 -0300

    DEM-8 refactor: estilos.

--- src/containers/home/Home.tsx
+++ src/containers/home/Home.tsx
@@ -2,7 +2,7 @@ import CrearPartida from "./components/CrearPartida";
 function Home() {
     return (
         <div className="flex h-[100vh] w-full items-center justify-center">
-            <CrearPartida /> 
+            <CrearPartida />
         </div>
     );
 }
