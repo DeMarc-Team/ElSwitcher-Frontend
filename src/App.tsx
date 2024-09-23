483c942780304e6a4104e836651b8264b9ff8b76
kidmand <daian.gimenez@mi.unc.edu.ar>
2024-09-22 23:49:43 -0300

    se implementó la redirección al tablero al hacer click en iniciar partida

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "@/containers/home/Home";
 import SalaEspera from "./containers/partida_sala_espera/SalaEspera";
+import Dashboard from "./containers/dashboard/components/Board";
 
 function App() {
     return (
         <div className="px-16 py-5">
             <Router>
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route 
+                        path="/" 
+                        element={<Home />} 
+                    />
                     <Route
                         path="/partidas/:id_partida/sala-espera"
                         element={<SalaEspera />}
+                    />
+                    <Route 
+                        path="/partidas/dashboard"
+                        element={<Dashboard />}
                     />
                 </Routes>
             </Router>
