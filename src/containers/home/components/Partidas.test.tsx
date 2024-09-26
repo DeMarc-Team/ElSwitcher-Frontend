fd4670d7060c2b4cf665c09290da5fc237fd7a80
Camila <camila.nanini.2307@gmail.com>
2024-09-26 00:00:41 -0300

    add: Se crearon dos test más para el listar partida

--- src/containers/home/components/Partidas.test.tsx
+++ src/containers/home/components/Partidas.test.tsx
@@ -1,15 +1,39 @@
-import { describe,expect, test} from 'vitest'
+import { describe,expect, test, vi} from 'vitest'
 import { render, screen} from '@testing-library/react';
 import Partidas from './Partidas';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('Partidas Component', () => {
+
     test('Se renderiza la lista de partidas', () => {
         render(<Partidas />);
         expect(screen.getByText('Lista de partidas:')).toBeDefined();
     });
+
+    vi.mock('@/services/api/obtener_partidas', () => ({
+      ObtenerPartidas: vi.fn(() => Promise.resolve([]))
+    }));
+
+    test('No hay partidas', async () => {
+      render(<Partidas />);      
+      expect(await screen.getByText('No hay partidas creadas.')).toBeDefined();
+  });
+   
+  vi.mock('@/services/api/obtener_partidas', () => ({
+    ObtenerPartidas: vi.fn(() => Promise.resolve([
+      { id: 1, nombre_partida: 'Partida 1' },
+      { id: 2, nombre_partida: 'Partida 2' },]))
+  }));
+
+    test('Mocker las partidas', async () => {
+    //Simular la navegación pues partidas llama a FormUnirse
+      render(
+        <MemoryRouter>
+          <Partidas />
+        </MemoryRouter>
+      );
+    expect(await screen.findByText('Partida 1')).toBeDefined();
+    expect(await screen.findByText('Partida 2')).toBeDefined();
+  })
+     
 });
-
-
-
-// test('', () => {
-//   })
