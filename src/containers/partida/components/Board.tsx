6debd937681b623ea7915611d34ba1c6cac85364
Camila <camila.nanini.2307@gmail.com>
2024-10-06 19:34:53 -0300

    add: Ahora el tablero detecta las casillas que son parte de una figura (cuidado con el hardcodeo del endpoint)

--- src/containers/partida/components/Board.tsx
+++ src/containers/partida/components/Board.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { ObtenerTablero } from "../../../services/api/ver_tablero";
+import { ObtenerTablero, Casilla } from "../../../services/api/ver_tablero";
 import { cn } from "@/services/shadcn_lib/utils";
 
 const COLORES: string[] = [
@@ -15,11 +15,13 @@ interface DashboardProps {
 
 const Board: React.FC<DashboardProps> = ({ id_partida }) => {
     const [tablero, setTablero] = useState<number[][]>([]);
+    const [figuras, setFiguras] = useState<Casilla[][]>([]);
 
     const fetchTablero = async () => {
         try {
             const data = await ObtenerTablero(id_partida);
             setTablero(data.tablero6x6);
+            setFiguras(data.figuras);
         } catch (error) {
             console.error("Error al obtener el tablero:", error);
         }
@@ -29,19 +31,41 @@ const Board: React.FC<DashboardProps> = ({ id_partida }) => {
         fetchTablero();
     }, [id_partida]);
 
+    function handleRenderCell(
+        cell: number,
+        rowIndex: number,
+        colIndex: number
+    ) {
+        let esParteDeFigura = false;
+        figuras.forEach((fila) => {
+            fila.forEach((casilla) => {
+                if (casilla.row === rowIndex && casilla.column === colIndex) {
+                    esParteDeFigura = true; 
+                }
+            });
+        });
+
+        return (
+            <button
+                key={`${rowIndex}-${colIndex}`}
+                className={cn(
+                    "flex h-12 w-12 items-center justify-center rounded-lg bg-blue-400 shadow-lg hover:scale-110 hover:border-indigo-500",
+                    `bg-${COLORES[cell - 1]}-400`,
+                    esParteDeFigura
+                        ? "border-4 border-blue-600"
+                        : "border-2 border-black"
+                )}
+            ></button>
+        );
+    }
+
     return (
         <div className="flex h-fit w-[388px] items-center justify-center">
             <div className="grid grid-cols-6 grid-rows-6 gap-1 rounded-lg border-4 border-black bg-yellow-100 p-2 shadow-2xl">
                 {tablero.map((row, rowIndex) =>
-                    row.map((cell, colIndex) => (
-                        <button
-                            key={`${rowIndex}-${colIndex}`}
-                            className={cn(
-                                "flex h-12 w-12 items-center justify-center rounded-lg border-2 border-black bg-blue-400 shadow-lg hover:scale-110 hover:border-indigo-500",
-                                `bg-${COLORES[cell - 1]}-400`
-                            )}
-                        ></button>
-                    ))
+                    row.map((cell, colIndex) =>
+                        handleRenderCell(cell, rowIndex, colIndex)
+                    )
                 )}
             </div>
         </div>
--- src/services/api/ver_tablero.ts
+++ src/services/api/ver_tablero.ts
@@ -1,37 +1,84 @@
 import { API_HOST } from "./const";
+interface Casilla {
+    row: number;
+    column: number;
+}
 
 interface Tablero {
     tablero6x6: number[][];
+    figuras: Casilla[][];
 }
 
 const ObtenerTablero = async (id_partida: number): Promise<Tablero> => {
+    // try {
+    //     const response = await fetch(
+    //         `${API_HOST}/juego/${id_partida}/tablero`,
+    //         {
+    //             method: "GET",
+    //             headers: {
+    //                 "Content-Type": "application/json",
+    //             },
+    //         }
+    //     );
+
+    //     if (!response.ok) {
+    //         throw new Error(
+    //             `Fallo obteniendo el tablero!  Status: ${response.status}`
+    //         );
+    //     }
+
+    //     const data = await response.json();
+    //     return {
+    //         tablero6x6: data.tablero6x6,
+    //         figuras: data.figuras,
+    //     };
+    // } catch (error) {
+    //     console.error("Error talero:", error);
+    //     throw error;
+    // }
     try {
-        const response = await fetch(
-            `${API_HOST}/juego/${id_partida}/tablero`,
-            {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            }
-        );
+        // Hardcodear valores
+        const tablero6x6 = [
+            [3, 1, 2, 3, 4, 1],
+            [1, 2, 3, 4, 1, 2],
+            [2, 3, 4, 1, 2, 3],
+            [3, 4, 1, 2, 3, 4],
+            [4, 1, 2, 3, 4, 1],
+            [1, 2, 3, 4, 1, 2],
+        ];
 
-        if (!response.ok) {
-            throw new Error(
-                `Fallo obteniendo el tablero!  Status: ${response.status}`
-            );
-        }
+        const figuras = [
+            [
+                { row: 0, column: 0 },
+                { row: 0, column: 1 },
+            ],
+            [
+                { row: 1, column: 2 },
+                { row: 1, column: 3 },
+            ],
+            [
+                { row: 2, column: 4 },
+                { row: 2, column: 5 },
+            ],
+            [
+                { row: 3, column: 2 },
+                { row: 3, column: 3 },
+                { row: 3, column: 4 },
+            ],
+        ];
 
+        // Devuelve los valores hardcodeados
         return {
-            tablero6x6: await response.json(),
+            tablero6x6,
+            figuras,
         };
     } catch (error) {
-        console.error("Error talero:", error);
+        console.error("Error obteniendo tablero:", error);
         throw error;
     }
 };
 
-export { ObtenerTablero, type Tablero };
+export { ObtenerTablero, type Tablero, type Casilla };
 
 // const TABLERO_MANUAL: Tablero = {
 //     tablero6x6: [
