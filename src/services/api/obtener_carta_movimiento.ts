560244a71380e34b0e751b2a7f5396e622f20022
kidmand <daian.gimenez@mi.unc.edu.ar>
2024-10-11 19:03:15 -0300

    DEM-87 feat: se corrigio la respuesta del fetch obtenerCartaMovimiento

--- src/services/api/obtener_carta_movimiento.ts
+++ src/services/api/obtener_carta_movimiento.ts
@@ -10,7 +10,7 @@ const ObtenerCartasMovimientos = async (
     id_jugador: number
 ): Promise<CartaMovimientoResponse[]> => {
     try {
-        /*const response = await fetch(
+        const response = await fetch(
             `${API_HOST}/juego/${id_partida}/jugadores/${id_jugador}/cartas_movimiento`,
             {
                 method: "GET",
@@ -25,9 +25,16 @@ const ObtenerCartasMovimientos = async (
                 `Fallo obteniendo las cartas movimientos!  Status: ${response.status}`
             );
         }
+        let data = await response.json();
 
-        return await response.json();*/
-        return mockCartasMovimiento;
+        // TODO: cuando este el back, quitar esto:
+        data = data.map((carta: any) => {
+            return {
+                movimiento: carta.movimiento,
+                parcialmente_usada: false,
+            };
+        });
+        return data;
     } catch (error) {
         console.error("Error cartas de movimientos:", error);
         throw error;
