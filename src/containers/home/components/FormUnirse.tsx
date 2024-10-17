b850221c13464ff97ffe54d0fb931e060729aad3
Camila <camila.nanini.2307@gmail.com>
2024-10-17 00:13:22 -0300

    fix:Una linea más de un componente

--- src/containers/home/components/FormUnirse.tsx
+++ src/containers/home/components/FormUnirse.tsx
@@ -6,7 +6,6 @@ import {
     DialogDescription,
     DialogTitle,
     DialogTrigger,
-    DialogDescription,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
