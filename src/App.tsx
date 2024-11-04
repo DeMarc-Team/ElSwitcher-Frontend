31bf2d655866c769b33a39d6f9a3d6188c6ba70c
Camila <camila.nanini.2307@gmail.com>
2024-11-04 00:54:43 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,49 @@ import { PartidaWebsocketProvider } from "@/context/PartidaWebsocket";
 import { PartidaProvider } from "@/context/PartidaContext";
 import { MovimientoContextProvider } from "@/context/UsarCartaMovimientoContext";
 import { FiguraContextProvider } from "@/context/UsarCartaFiguraContext";
+import { useEffect, useState } from "react";
+
+interface CtrlAComponentProps {
+    setIsCtrlA: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const CtrlAComponent: React.FC<CtrlAComponentProps> = ({ setIsCtrlA }) => {
+    useEffect(() => {
+        const handleKeyDown = (event: KeyboardEvent) => {
+            // Verifica si Ctrl + A fue presionado
+            if (event.ctrlKey && event.key === 'a') {
+                event.preventDefault(); 
+                setIsCtrlA(true);
+            }
+        };
+        window.addEventListener('keydown', handleKeyDown);
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [setIsCtrlA]);
+    return null;
+};
 
 function App() {
+    const [isCtrlA, setIsCtrlA] = useState(false);
+
     return (
-        <div className="px-32 max-md:px-10">
+        <div className={`px-32 max-md:px-10 ${isCtrlA ? 'emoji-cursor' : ''}`}>
+            <CtrlAComponent setIsCtrlA={setIsCtrlA} />
             <PartidaWebsocketProvider>
                 <Router>
                     <Routes>
                         <Route path="/" element={<Home />} />
-                        <Route
-                            path="/partidas/:id_partida/sala-espera"
-                            element={<SalaEspera />}
-                        />
-                        <Route
-                            path="/partidas/:id_partida"
-                            element={
-                                <PartidaProvider>
-                                    <MovimientoContextProvider>
-                                        <FiguraContextProvider>
-                                            <Partida />
-                                        </FiguraContextProvider>
-                                    </MovimientoContextProvider>
-                                </PartidaProvider>
-                            }
-                        />
+                        <Route path="/partidas/:id_partida/sala-espera" element={<SalaEspera />} />
+                        <Route path="/partidas/:id_partida" element={
+                            <PartidaProvider>
+                                <MovimientoContextProvider>
+                                    <FiguraContextProvider>
+                                        <Partida />
+                                    </FiguraContextProvider>
+                                </MovimientoContextProvider>
+                            </PartidaProvider>
+                        } />
                     </Routes>
                 </Router>
                 <Toaster />
