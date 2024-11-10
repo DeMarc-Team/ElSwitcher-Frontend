3634b62b787945dfa8dc1368fc5370800000698a
Ramiro Lugo <ramiro.lugo@mi.unc.edu.ar>
2024-11-10 10:42:31 -0300

    DEM-124 feat: cambiando los colores de mensajes de log.

--- src/containers/partida/components/ChatMensajes.tsx
+++ src/containers/partida/components/ChatMensajes.tsx
@@ -16,16 +16,25 @@ const ChatMensajes: React.FC<ChatMensajeProps> = ({
     return (
         <ScrollArea className="mb-1 h-[267px] rounded-lg border-2 border-black bg-white px-1">
             <ul>
-                {messagesList.map((msg, i) => (
-                    <li
-                        key={`chat-message-${i}`}
-                        className={`my-2 table max-w-[140px] text-wrap break-all rounded-md border-2 border-black p-2 text-sm ${msg.id_jugador === id_jugador ? "ml-auto bg-green-400" : "bg-blue-400"}`}
-                    >
-                        <span className="text-xs text-slate-900">
-                            {msg.message}
-                        </span>
-                    </li>
-                ))}
+                {messagesList.map((msg, i) => {
+                    let messageClass = "bg-red-400";
+                    if (msg.type_message == "USER") {
+                        messageClass =
+                            msg.id_jugador === id_jugador
+                                ? "ml-auto bg-green-400"
+                                : "bg-blue-400";
+                    }
+                    return (
+                        <li
+                            key={`chat-message-${i}`}
+                            className={`my-2 table max-w-[140px] text-wrap break-all rounded-md border-2 border-black p-2 text-sm ${messageClass}`}
+                        >
+                            <span className="text-xs text-slate-900">
+                                {msg.message}
+                            </span>
+                        </li>
+                    );
+                })}
                 <p ref={messagesEndRef} />
             </ul>
         </ScrollArea>
